9094c1a6d4ca9de357f3b9c49f3b55527364a494
AndersonBillss <anderson@thebillsfamily.com>
2024-10-14 22:24:05 -0600

    created app

---
+++ src/routes/folder/test.tsx
@@ -0,0 +1,10 @@
+import { Title } from "@solidjs/meta";
+
+export default function Home() {
+    return(
+        <main>
+            <Title>Hello World</Title>
+            <h1>Hello World</h1>
+        </main>
+    )
+}
